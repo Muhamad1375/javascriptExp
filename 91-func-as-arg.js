87c49ced7d00dd24599d9d36b253538a6aa41204
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-12-20 02:24:36 +0330

    91 func as arg

---
+++ 91-func-as-arg.js
@@ -0,0 +1,31 @@
+function callThreeTimes(f) {
+      f();
+      f();
+      f();
+}
+
+function cry() {
+      console.log("BOO HOO I'M SO SAD!");
+}
+
+function rage() {
+      console.log("I HATE EVERYTHING!");
+}
+//////////////////////
+function repeatNTimes(action, num) {
+      for(let i = 0; i < num; i++){
+            action();
+      }
+}
+
+repeatNTimes(rage, 13);
+////////////////////////
+
+function pickOne(f1,f2) {
+      let rand = Math.random();
+      if (rand<0.5){
+            f1();
+      } else {
+            f2();
+      }
+}
