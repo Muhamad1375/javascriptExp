cd36bb8e299847f5c05a6e5de37b0c0f5869a475
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-12-20 02:05:21 +0330

    93 callback

---
+++ 93-callback.js
@@ -0,0 +1,9 @@
+function grumpus() {
+      alert("GAHHH GO AWAY!")
+}
+
+// setTimeout(callback, delay)
+setTimeout(function(){ //we pass an anonymous callback function
+      alert("WELCOME!"); 
+}, 3000);
+
